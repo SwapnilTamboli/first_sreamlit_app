1bf8d1f9c39435fb78c67d077e4a09fcf7d347d3
SwapnilTamboli <104581513+SwapnilTamboli@users.noreply.github.com>
2022-05-02 16:30:56 +0100

    Update sreamlit_app.py

--- sreamlit_app.py
+++ sreamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title('Breakfast')
 
@@ -18,4 +19,7 @@ fruits_selected = streamlit.multiselect("Pick Some Fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
+
+fruityvice_rerponse = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_rerponse)
 
